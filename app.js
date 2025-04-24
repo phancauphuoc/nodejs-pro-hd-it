4b688c09dbf302a6a5fa4c0bd2620333cc0176f8
Phuoc Phuoc <phancauphuoc@gmail.com>
2025-04-24 22:19:33 +0700

    video 24

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("hello work");
+const express = require('express');
+
+const app = express();
+
+const PORT = 8080;
+
+app.get("/", (req, res) => {
+    res.send("Hello Word");
+})
+
+app.get("/phuoc", (req, res) => {
+    res.send("Hello Phuoc");
+})
+
+app.listen(PORT, () => {
+    console.log(`My app is running on port: ${PORT}`)
+});
