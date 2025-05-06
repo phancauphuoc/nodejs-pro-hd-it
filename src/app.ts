7730278f50f7754dd4aa40a759025923af488055
Phuoc Phuoc <phancauphuoc@gmail.com>
2025-05-06 22:52:36 +0700

    video 32: setup ejs

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,14 @@ import express from 'express';
 import 'dotenv/config';
 
 const app = express();
-
 const PORT = process.env.PORT || 8080;
 
+//config view engine
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
 app.get("/", (req, res) => {
-    res.send("Hello Word Update11");
+    res.render("home.ejs");
 })
 
 app.get("/phuoc", (req, res) => {
