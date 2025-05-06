484d8efc3b5c4367e1ab41a19379202c3535fd96
Phuoc Phuoc <phancauphuoc@gmail.com>
2025-05-06 22:26:35 +0700

    video 29 setup nodemon

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ const app = express();
 const PORT = 8080;
 
 app.get("/", (req, res) => {
-    res.send("Hello Word Update");
+    res.send("Hello Word Update11");
 })
 
 app.get("/phuoc", (req, res) => {
