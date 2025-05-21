9c37e1023260baf5d6f8910629df0934cb866b8c
Phuoc Phuoc <phancauphuoc@gmail.com>
2025-05-21 21:59:33 +0700

    video 39: connect sql local

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@
 import express from 'express';
 import 'dotenv/config';
 import webRoutes from './routes/web';
+import getConnection from './config/database';
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -19,6 +20,8 @@ app.use(express.static('public'));
 
 //config routes
 webRoutes(app);
+
+getConnection();
 
 app.listen(PORT, () => {
     console.log(`My app is running on port123: ${PORT}`)
